65fd8a2de70c850b7b1c705e9b88516a83a3f5f1
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-02 00:23:41 +0530

    Refactor middleware for improved readability and maintainability

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["sign-in", "sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
+  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up", "/", "/verify/:path*"],
 };
 
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
   if (
     token &&
     (url.pathname.startsWith("/sign-in") ||
@@ -19,6 +21,7 @@ export async function middleware(request: NextRequest) {
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
+
   if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
