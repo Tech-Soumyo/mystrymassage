4c1fb9e5bc1f0f0901f79723a2869f091fc733f5
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-01 13:20:22 +0530

    Add NextAuth options with credentials provider for authentication

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,44 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect.lib";
+import UserModel from "@/model/user.model";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("Please verify your Account before login");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incoreect Password");
+          }
+        } catch (err: any) {
+          throw new Error("No user found with Email", err);
+        }
+      },
+    }),
+  ],
+};
