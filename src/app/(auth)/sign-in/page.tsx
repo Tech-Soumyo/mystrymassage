93ca9fa63f6eea27446a6dda495a1319f240e51b
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-02 11:31:36 +0530

    Implement authentication with NextAuth: add sign-in/sign-out functionality and AuthProvider context

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-function page() {
-  return <div>page</div>;
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button
+        className="bg-orange-600 px-3 py-2 m-4 rounded"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
+    </>
+  );
 }
-
-export default page;
