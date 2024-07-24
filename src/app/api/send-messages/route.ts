4be6688a2dc3497e9b7049e6bdf14da80e45e56f
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-25 05:12:00 +0530

    Implement POST endpoint for sending messages. Includes checks for user existence and message acceptance.

--- src/app/api/send-messages/route.ts
+++ src/app/api/send-messages/route.ts
@@ -1,14 +1,17 @@
 import dbConnect from "@/lib/dbConnect.lib";
 import UserModel from "@/model/user.model";
 import { Message } from "@/model/user.model";
-import { use } from "react";
 
 export async function POST(request: Request) {
   await dbConnect();
 
+  // Extract username and message content from request body
   const { username, content } = await request.json();
   try {
+    // Find user by username
     const user = await UserModel.findOne({ username });
+
+    // Handle user not found
     if (!user) {
       return Response.json(
         {
@@ -18,7 +21,7 @@ export async function POST(request: Request) {
         { status: 404 }
       );
     }
-    // is user accepting the messages
+    // Check if user is accepting messages
     if (!user.isAcceptingMessege) {
       return Response.json(
         {
@@ -29,10 +32,16 @@ export async function POST(request: Request) {
       );
     }
 
+    // Create a new message object
     const newMessage = { content, createdAt: new Date() };
+
+    // Add the new message to the user's messages array
     user.messages.push(newMessage as Message);
+
+    // Save the updated user document
     await user.save();
 
+    // Respond with success message
     return Response.json(
       {
         success: true,
