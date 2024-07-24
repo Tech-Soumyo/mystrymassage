5801b1776c0f3262b408e17542b050c035590941
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-25 04:19:17 +0530

    perform aggregation pipelining

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -6,11 +6,16 @@ import { User } from "next-auth";
 import mongoose from "mongoose";
 
 export async function GET(request: Request) {
+  // Connect to the MongoDB database
   await dbConnect();
 
+  // Get the session data from NextAuth.js
   const session = await getServerSession(authOptions);
+
+  // User can be undefined if not authenticated
   const user: User = session?.user as User;
 
+  // Check if the user is authenticated
   if (!session || !session.user) {
     return Response.json(
       {
@@ -23,12 +28,41 @@ export async function GET(request: Request) {
     );
   }
 
+  // Convert the user ID to a MongoDB ObjectId (assuming user._id is a string)
   const userId = new mongoose.Types.ObjectId(user._id);
   try {
+    // Perform aggregation to fetch user's messages
     const user = await UserModel.aggregate([
-        {}
-    ])
-  } catch (error) {
-    
-  }
+      {
+        $match: { if: userId },
+      },
+      {
+        $unwind: "$messages",
+      },
+      {
+        $sort: { "messages.createdAt": -1 },
+      },
+      {
+        $group: { _id: "$_id", messages: { $push: "$messafes" } },
+      },
+    ]);
+    // Handle user not found or no messages
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User nor found",
+        },
+        { status: 401 }
+      );
+    }
+    // Respond with success message and user's messages
+    return Response.json(
+      {
+        success: true,
+        messages: user[0].messages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {}
 }
